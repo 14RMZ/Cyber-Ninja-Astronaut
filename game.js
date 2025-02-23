68aed816c8efd8fd584452e612b282038a647d11
14RMZ <mineai.rmz@gmail.com>
2025-02-24 04:11:02 +0500

    Update game.js

--- game.js
+++ game.js
@@ -14,11 +14,11 @@ highScore = parseInt(highScore);
 
 // Load the background image
 const backgroundImage = new Image();
-backgroundImage.src = "https://14rmz.github.io/Cyber-Ninja-Astronaut/sprite_sheet.png";
+backgroundImage.src = "GameBackground.jpg";
 
 // Load the player sprite sheet
 const playerSpriteSheet = new Image();
-playerSpriteSheet.src = "https://i.imgur.com/3QZQZ.png"; // Replace with your sprite sheet path
+playerSpriteSheet.src = "https://14rmz.github.io/Cyber-Ninja-Astronaut/sprite_sheet.png"; // Replace with your sprite sheet path
 
 
 // Player animation frames
